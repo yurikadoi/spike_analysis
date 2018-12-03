25b3943379fbbd6bc4f7526884ded26e54d10b42
Yurika DOI <kuroshibashion@gmail.com>
2018-12-03 18:46:21 -0500

    first commit. scatter plot the patch residence time (PRT) against trial number to see the trend during a session

---
+++ PRT_trialnum_scatter.m
@@ -0,0 +1,14 @@
+figure; scatter(1:length(PRTsSm),PRTsSm); lsline
+xlabel('trial #')
+ylabel('PRT (Patch Residence Time)')
+title('PRT for Sm trials')
+
+figure; scatter(1:length(PRTsMd),PRTsMd); lsline
+xlabel('trial #')
+ylabel('PRT (Patch Residence Time)')
+title('PRT for Md trials')
+
+figure; scatter(1:length(PRTsLg),PRTsLg); lsline
+xlabel('trial #')
+ylabel('PRT (Patch Residence Time)')
+title('PRT for Lg trials')
